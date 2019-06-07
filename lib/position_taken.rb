5466eb5ea9e73728d12f9e658b68a99d271ef258
Michael McCullough <michael.mccullough21@gmail.com>
2019-06-07 23:48:46 +0000

    Automatically backed up by Learn

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -1,6 +1,6 @@
 board = [" ", " ", " ", " ", " ", " ", " ", " ", " "]
 def position_taken?(board, index)
-   if !(board[index] == " ") || !(board[index] == "") 
+   if !(board[index] == " ") || !(board[index] == "")
       board[index] == "X" || board[index] == "O"
    end
 end
